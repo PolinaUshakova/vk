57cb967dd45be2ef786f223a5e85fc21e0bbda1e
Banana Android Z <ndrdbnnz@gmail.com>
2018-01-25 20:55:30 +0300

    first commit

---
+++ app/src/main/java/com/project/myapplication/LaunchActivity.java
@@ -0,0 +1,25 @@
+package com.project.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+
+    }
+    public void onButtonClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
